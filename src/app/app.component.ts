55e88b3ab20ffb8af3aca12b70b27a17be89785c
spirithuntt <nouhaila.elhousnii@gmail.com>
2023-12-14 13:50:10 +0100

    configuration: done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private modalService: NgbModal) {
-  }
+
+  constructor(private http: HttpClient, private modalService: NgbModal) {}
 
   public open(modal: any): void {
     this.modalService.open(modal);
