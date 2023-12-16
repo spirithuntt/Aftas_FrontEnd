7855d8e0d0efa713f3537bae3a4e4ee96e8b3f4d
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2023-12-16 18:53:26 +0100

    Update register-member.component.ts

--- src/app/components/register-member/register-member.component.ts
+++ src/app/components/register-member/register-member.component.ts
@@ -14,6 +14,40 @@ import { RegisterMemberService } from 'src/app/services/members/register-member.
   standalone: true,
   imports: [ReactiveFormsModule]
 })
-export class RegisterMemberComponent {
+export class RegisterMemberComponent implements OnInit {
+  registrationForm!: FormGroup; // Add the '!' operator to indicate that it will be initialized in ngOnInit
+  constructor(private formBuilder: FormBuilder, private registerMemberService: RegisterMemberService) { }
 
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      member: ['', Validators.required],
+      competition: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      const registerMemberData: RegisterMember = {
+        member: this.registrationForm.value.member,
+        competition: this.registrationForm.value.competition,
+      };
+
+      const registerMemberModel = new RegisterMemberC(
+        registerMemberData.member,
+        registerMemberData.competition
+      );
+
+      // Call the API to register the member in the competition
+      this.registerMemberService.registerMember(registerMemberModel).subscribe(
+        (response: Response) => {
+          console.log('Member successfully registered for the competition', response);
+          // Handle success here, e.g., show a success message to the user
+        },
+        (error: any) => {
+          console.error('Error registering member for the competition', error);
+          // Handle error, e.g., display an error message to the user
+        }
+      );
+    }
+  }
 }
