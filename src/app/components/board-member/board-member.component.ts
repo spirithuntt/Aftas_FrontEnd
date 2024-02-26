eea91cfd7982470f12359e6467879dff2656e864
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2024-02-26 04:11:28 +0100

    Add Board Member

--- src/app/components/board-member/board-member.component.ts
+++ src/app/components/board-member/board-member.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CompetitionService } from '../../services/competition/competition.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board-member',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board-member.component.html',
   styleUrl: './board-member.component.css'
 })
 export class BoardMemberComponent {
+  competitions: any[] = [];
+  constructor(private competitionService: CompetitionService) { }
+
+  ngOnInit(): void {
+    this.competitionService.getAllMemberCompetitions().subscribe(
+      (data) => {
+        this.competitions = data.data;
+        console.log(this.competitions);
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+      }
+    );
+  }
 
 }
--- src/app/services/competition/competition.service.ts
+++ src/app/services/competition/competition.service.ts
@@ -3,18 +3,19 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Competition } from '../../models/competition';
 import { ResponseModel } from '../../core/request/response.model';
+import { Member } from './../../models/member';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompetitionService {
 
-  private apiUrl = "http://127.0.0.1:8080/api/v1/competitions";
+  private apiUrl = "http://127.0.0.1:8080/api/v1";
 
   constructor(private http: HttpClient) { }
 
   getCompetitions(): Observable<Competition[]>{
-    return this.http.get<Competition[]>(this.apiUrl).pipe(
+    return this.http.get<Competition[]>(this.apiUrl + `/competitions`).pipe(
         map((res: any) => res.data)
     )
   }
@@ -31,4 +32,10 @@ export class CompetitionService {
   addCompetition(competition: Competition): Observable<ResponseModel<Competition>>{
     return this.http.post<ResponseModel<Competition>>(this.apiUrl, competition);
   }
+
+  getAllMemberCompetitions(): Observable<any> {
+    const url = `${this.apiUrl}/ranking/competitions/member`;
+    return this.http.get(url);
+  }
+
 }
